a7673c4654819ac2d42f27911a4a86af96b1b4a4
prkrsamuels7 <prkrsamuels@protonmail.com>
2022-07-18 19:34:46 -0400

    store items and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+import { Container } from 'react-bootstrap'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Store } from './pages/Store'
+import { NavBar } from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>
-      hello
-    </h1>
+    <>
+      <NavBar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   )
 }
 
